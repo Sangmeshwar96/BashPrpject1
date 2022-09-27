33303ec7e174a33481bef3f241507d3b078373a2
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-09-27 08:04:29 +0530

    Array Example1

---
+++ array1.sh
@@ -0,0 +1,12 @@
+i=0
+s[((i++))]="Nithin"
+s[((i++))]="Shruthi"
+s[((i++))]="Nikitha"
+s[((i++))]="Sangmeshwar"
+s[((i++))]="Balaji"
+s[((i++))]="Basha"
+
+for i in ${s[@]}
+do
+        echo  $i
+done
